7cc487efcf6369c6f562aa1584a3cdc8f1438ee9
Myron Walker <myron@Myrons-Mac-mini.local>
2024-08-10 21:15:56 -0700

    Update runtimevariables to match configurationvariables rename

--- source/packages/mojo/runtime/runtimevariables.py
+++ source/packages/mojo/runtime/runtimevariables.py
@@ -22,7 +22,7 @@ from uuid import uuid4
 from mojo.collections.contextpaths import ContextPaths
 from mojo.collections.wellknown import ContextSingleton
 
-from mojo.configuration.variables import MOJO_CONFIG_VARIABLES, resolve_configuration_variables
+from mojo.configuration.configurationvariables import MOJO_CONFIG_VARIABLES, resolve_configuration_variables
 
 from mojo.xmods.xconvert import parse_bool
 from mojo.xmods.xdatetime import (
